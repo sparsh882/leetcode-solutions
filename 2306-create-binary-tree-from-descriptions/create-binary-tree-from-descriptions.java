127ac27778a3facf09e24a9569c2237ef64ee87a
sparshawasthi <awasthisparsh882@gmail.com>
2026-06-07 22:46:16 +0530

    Time: 66 ms (73.78%) | Memory: 86.2 MB (50.74%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.java
@@ -0,0 +1,40 @@
+
+
+/******************************************************** JAVA ********************************************************/
+//Simple Approach using map and set
+//T.C : O(n), where n is the size of descriptions
+//S.C : O(n), size of map and set
+class Solution {
+    public TreeNode createBinaryTree(int[][] descriptions) {
+        Map<Integer, TreeNode> mp = new HashMap<>();
+        Set<Integer> childSet = new HashSet<>();
+
+        // Create nodes and set up parent-child relationships
+        for (int[] description : descriptions) {
+            int parent = description[0];
+            int child = description[1];
+            boolean isLeft = description[2] == 1;
+
+            mp.putIfAbsent(parent, new TreeNode(parent));
+            mp.putIfAbsent(child, new TreeNode(child));
+
+            if (isLeft) {
+                mp.get(parent).left = mp.get(child);
+            } else {
+                mp.get(parent).right = mp.get(child);
+            }
+
+            childSet.add(child);
+        }
+
+        // Find the root node (which is not any child)
+        for (int[] description : descriptions) {
+            int parent = description[0];
+            if (!childSet.contains(parent)) {
+                return mp.get(parent);
+            }
+        }
+
+        return null;
+    }
+}
